dc6c132f4438df8a69e020867579629ea452a807
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-11 17:00:06 -0600

    Solve 99 bottles

---
+++ 99bottles.js
@@ -0,0 +1,10 @@
+// 99 bottles
+for (var beer = 99; beer >= 1; beer--) {
+  if (beer === 1) {
+    var bottles = 'bottle';
+  } else {
+    bottles = "bottles";
+  }
+    var beerBottles = beer + " " + bottles + " of beer on the wall, " + beer + " " + bottles + " of beer, you take one down, pass it around, " + beer + " " + bottles + " of beer on the wall!";
+    console.log(beerBottles);
+}
